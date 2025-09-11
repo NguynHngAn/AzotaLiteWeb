3bdb284bb8cecbcd19112bf036bf80630f8c0191
HongAn <nguyenhongan.tphcm@gmail.com>
2025-09-11 22:49:51 +0700

    Giai doan 1 - buoc 1\

---
+++ AzotaLite.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AzotaLite.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
